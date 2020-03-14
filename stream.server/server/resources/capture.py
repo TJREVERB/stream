259ad675d3251ee1a218119ec323b7626ee1b00c
Anup Bagali <anup.bagali1@gmail.com>
2020-03-14 13:10:16 -0400

    added post endpoint

--- stream.server/server/resources/capture.py
+++ stream.server/server/resources/capture.py
@@ -1,8 +1,10 @@
 import os
 import glob
+import base64
 from flask.json import jsonify
 from flask_restful import Resource
 
+from ..utils import validate, BodyArg
 from ..settings import IMAGES_DIR
 
 
@@ -17,3 +19,12 @@ class CaptureResource(Resource):
                 "image": [file[file.find('static'):] for file in files]
             }
         )
+
+    @staticmethod
+    @validate(
+        BodyArg("filename", type=str, help="Filename of image"),
+        BodyArg("image", type=str, help="base64 encoded image"),
+    )
+    def post(**kwargs):
+        with open(kwargs['filename'], 'wb') as w:
+            w.write(base64.decodebytes(kwargs['image']))
