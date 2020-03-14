5c99d8575012541e80f35072dda3d5eff3fd84d5
Anup Bagali <anup.bagali1@gmail.com>
2020-03-14 13:05:55 -0400

    tested camera

--- stream.camera/capture.py
+++ stream.camera/capture.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 import os
-import cv2
 import sys
 import time
-import socket
+import base64
 import datetime
-from picamera import PiCamera
+import requests
+from fakecamera import PiCamera
+#from picamera import PiCamera
 
-IMAGES_DIR = "captures"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGES_DIR = os.path.abspath(os.path.join(BASE_DIR, "captures"))
 IMAGE_TYPE = "png"
 RESOLUTION = (1024, 768)
-TARGET = ("198.38.16.84", 5000)  # (host, port)
+HOST = "198.38.16.84"
+PORT = 5000
+SERVER_ENDPOINT = "http://{host}:{port}/captures".format(host=HOST, port=PORT)
 DELAY = 5
-VID_FILENAME = "../../stream_vid.mp4"
+
 
 class Camera:
     def __init__(
-        self,
-        resolution: tuple,
-        s: socket.socket,
-        images_dir: str,
-        image_type: str,
-        delay: int,
+            self,
+            resolution: tuple,
+            endpoint: str,
+            images_dir: str,
+            image_type: str,
+            delay: int,
     ):
         self.camera = PiCamera()
-        self.socket = s
+        self.endpoint = endpoint
         self.images_dir = images_dir
         self.image_type = image_type
         self.delay = delay
@@ -35,86 +40,26 @@ class Camera:
         self.camera.resolution(resolution)
         self.camera.start_preview()
         time.sleep(2)
-    
+
     def capture(self):
         timestamp = datetime.datetime.fromtimestamp(
             time.time()
         ).strftime(
             "%Y-%m-%d %H:%M:%S"
         )
-        filename = f"{self.images_dir}/{timestamp}.{self.image_type}"
+        filename = os.path.abspath(os.path.join(IMAGES_DIR, f"{self.images_dir}/{timestamp}.{self.image_type}"))
         self.camera.capture(filename)
         return filename
 
-
     def process(self):
-        filename = self.capture()
-        self.socket.send(cv2.imencode("." + self.image_type, cv2.imread(filename))[1].tobytes())
+        with open(self.capture(), 'rb') as f:
+            requests.post(self.endpoint, data={"filename": filename, "image": base64.encodebytes(f.read())})
         time.sleep(self.delay)
-
-
-class PseudoCamera:
-    def __init__(
-        self,
-        vid_path,
-        s: socket.socket,
-        images_dir,
-        image_type,
-        delay: int,
-    ):
-        self.vid_path = vid_path
-        self.socket = s
-        self.images_dir = images_dir
-        self.image_type = image_type
-        self.delay = delay
-
-        if not os.path.exists(self.images_dir):
-            os.mkdir(self.images_dir)
-        
-        self.cap = cv2.FileCapture(self.vid_path)
-        time.sleep(2)
-    
-    def capture(self):
-        timestamp = datetime.datetime.fromtimestamp(
-            time.time()
-        ).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-        filename = f"{self.images_dir}/{timestamp}.{self.image_type}"
-        ret, frame = self.cap.read()
-        counts = 0
-        while ret == False:
-            counts += 1
-            if counts > 5:
-                self.cap.close()
-                self.cap = cv2.FileCapture(self.vid_path)
-            ret, frame = self.cap.read()
-        cv2.imwrite(filename, frame)
-        return filename
-
-
-    def process(self):
-        filename = self.capture()
-        self.socket.send(cv2.imencode("." + self.image_type, cv2.imread(filename))[1].tobytes())
-        time.sleep(self.delay)
-
-
-def init_socket(server):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(server)
-        print("Successfully created socket")
-        return s
-    except socket.error as err:
-        print(f"Failed to create socket: {err}")
-        sys.exit(1)
 
 
 def main():
-#    camera = Camera(RESOLUTION, init_socket(TARGET), IMAGES_DIR, IMAGE_TYPE, DELAY)
-    camera = PseudoCamera(init_socket(TARGET), IMAGES_DIR, IMAGE_TYPE, DELAY)
-    while True:
-        camera.process()
+    camera = Camera(RESOLUTION, SERVER_ENDPOINT, IMAGES_DIR, IMAGE_TYPE, DELAY)
+    camera.process()
 
 
 if __name__ == "__main__":
---
+++ stream.camera/fakecamera/__init__.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+
+class PiCamera:
+    def __init__(self):
+        self.test_pic = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.png")
+        print(self.test_pic)
+
+    def resolution(self, res):
+        return
+
+    def start_preview(self):
+        return
+
+    def capture(self, filename):
+        shutil.copyfile(self.test_pic, filename)
