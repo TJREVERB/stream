28e8e393f62aabedfdff429aa2a32c2501549399
Anup Bagali <anup.bagali1@gmail.com>
2020-03-13 22:56:20 -0400

    actually finish capture.py

--- stream.camera/capture.py
+++ stream.camera/capture.py
@@ -1,24 +1,49 @@
+#!/usr/bin/env python3
+import os
+import cv2
 import sys
-from time import sleep
-import socket as socketio
+import time
+import socket
+import datetime
 from picamera import PiCamera
 
+IMAGE_TYPE = "png"
+RESOLUTION = (1024, 768)
 MIDDLEMAN = ("localhost", 5000)  # (host, port)
-DELAY = 5  # Interval to capture on
+DELAY = 5
 
 
-def init_camera():
-    camera = PiCamera()
-    camera.resolution(1024, 768)
-    camera.start_preview()
-    sleep(2)
-    return camera
+class Camera:
+    def __init__(self, resolution: tuple, s: socket.socket, images_dir: str, image_type: str, delay: int):
+        self.camera = PiCamera()
+        self.socket = s
+        self.images_dir = images_dir
+        self.image_type = image_type
+        self.delay = delay
+
+        if not os.path.exists(self.images_dir):
+            os.mkdir(self.images_dir)
+
+        self.camera.resolution(resolution)
+        self.camera.start_preview()
+        time.sleep(2)
+
+    def capture(self):
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+        filename = f"{self.images_dir}/{timestamp}.{self.image_type}"
+        self.camera.capture(filename)
+
+        im = cv2.imread(filename)
+        res, im_png = cv2.imencode("." + self.image_type)
+        self.socket.send(img_png.tobytes())
+
+        time.sleep(self.delay)
 
 
-def init_socket():
+def init_socket(server):
     try:
-        s = socketio.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(MIDDLEMAN)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(server)
         print("Successfully created socket")
         return s
     except socket.error as err:
@@ -27,10 +52,8 @@ def init_socket():
 
 
 def main():
-    camera, socket = init_camera(), init_socket()
-    while True:
-        camera.capture(socket, "png")
-        sleep(DELAY)
+    camera = Camera(RESOLUTION, init_socket(MIDDLEMAN), IMAGE_TYPE, DELAY)
+
 
 
 if __name__ == "__main__":
