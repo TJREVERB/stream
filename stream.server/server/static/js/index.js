f3bd827ce24ae3bb151084280a7ee410e40c356c
Srikar <srikarg89@gmail.com>
2020-03-14 01:34:45 -0400

    Slider

--- stream.server/server/static/js/index.js
+++ stream.server/server/static/js/index.js
@@ -1,24 +1,39 @@
 let currfile = "";
+let filenames = [];
 function updateDisplay(){
     let url = 'https://cubesat-stream.sites.tjhsst.edu/captures'
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url, false); // false for synchronous request
     xmlHttp.send();
-    let json = JSON.parse(JSON.parse(xmlHttp.responseText));
+    let json = JSON.parse(xmlHttp.responseText);
     console.log(json);
     if(json.status != 0){
         console.log("ERROR REEE");
         return;
     }
-    let filepath = json.image;
-    if(filepath != currfile){
-        let display = document.getElementById("display");
-        display.src = filepath;
-        currfile = filepath;
+    let filepaths = json.image;
+    for(let i = 0; i < filepaths.length; i++){
+        let filepath = filepaths[i];
+        if(!filenames.includes(filepath)){
+            filenames.push(filepath);            
+        }
     }
+    let slider = document.getElementById("myRange");
+    slider.max = filenames.length;
+    slider.value = slider.max;
 }
 
 
 window.onload = function(){
     setInterval(updateDisplay, 500);
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+      let display = document.getElementById("display");
+      display.src = filenames[parseInt(this.value) - 1];
+    } 
 };
